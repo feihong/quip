ab0b9d1aed5bbe2210b4a02f3b01fe5650ab4d29
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-10 14:34:28 -0500

    Make classifiers a list instead of a tuple...
    … so that setup() will not choke on it in Python 3

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ if sys.version_info[0] == 2:
     requires.extend(['futures', 'pathlib2'])
 
 
-with open('quip/__init__.py') as fd:
+with open('quip/__init__.py') as fp:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
+                        fp.read(), re.MULTILINE).group(1)
 
-with open('README.rst') as fd:
-    readme = fd.read()
+with open('README.rst') as fp:
+    readme = fp.read()
 
 
 setup(
@@ -38,7 +38,7 @@ setup(
     install_requires=requires,
     license='Apache 2.0',
     zip_safe=False,
-    classifiers=(
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
@@ -47,6 +47,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ),
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
 )
