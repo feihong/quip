5aeb13d87d60f4f91fc770c52398f01d6ce1cd19
Feihong <feihong.hsu@gmail.com>
2016-06-15 22:07:35 -0500

    Put the stop button on the same row as the percent indicator and progress bar

--- examples/progress_bar/client.py
+++ examples/progress_bar/client.py
@@ -7,7 +7,7 @@ from quipclient import Client
 
 COLORS = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue',
 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'yellow',
-'amber', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey', 'black']
+'amber', 'orange', 'deep-orange', 'blue-grey']
 
 
 class MyClient(Client):
